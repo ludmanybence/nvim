4adb4a6868b2dfff59808620a2db47acb55c0f77
Bence Ludmány <bence.ludmany@singula.se>
2023-12-26 23:46:26 +0100

    Add telescope and harpoon

---
+++ lua/plugins/navigation.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        "ThePrimeagen/harpoon",
+        config = function()
+            vim.keymap.set("n", "<leader>a", require("harpoon.mark").add_file)
+            vim.keymap.set("n", "<C-e>", require("harpoon.ui").toggle_quick_menu)
+            for i = 1, 9 do
+                vim.keymap.set("n", "<leader>" .. i, function() require("harpoon.ui").nav_file(i) end)
+            end
+        end
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        config = function()
+            require("nvim-tree").setup {}
+            vim.keymap.set("n", "<C-n>", vim.cmd.NvimTreeToggle)
+        end
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'make',
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end,
+            },
+        },
+    },
+}
