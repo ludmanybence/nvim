2798a2767208fa624e9e4b9fff30d1e46c6bbb10
Bence Ludmány <bence.ludmany@singula.se>
2023-12-27 13:20:11 +0100

    Move lsp config to /plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,51 @@
 return {
     {
-        'williamboman/mason.nvim',
-        opts = { ui = { border = 'single' } },
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            {
+                'williamboman/mason.nvim',
+                opts = { ui = { border = 'single' } },
+            },
+            { 'williamboman/mason-lspconfig.nvim' },
+            { 'neovim/nvim-lspconfig' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/nvim-cmp' },
+            { 'L3MON4D3/LuaSnip' },
+        },
+        config = function()
+            local lsp_zero = require('lsp-zero')
+
+            lsp_zero.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({ buffer = bufnr })
+            end)
+
+            require('lspconfig').lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { 'vim' }
+                        }
+                    }
+                }
+            })
+
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    'lua_ls',
+                    'vimls',
+                    'tsserver',
+                    'jsonls',
+                    'rust_analyzer',
+                    'omnisharp',
+                },
+                handlers = {
+                    lsp_zero.default_setup,
+                },
+            })
+        end
     },
-    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
-    { 'williamboman/mason-lspconfig.nvim' },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'L3MON4D3/LuaSnip' },
 }
