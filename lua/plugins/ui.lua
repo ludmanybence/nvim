e07f59830cdaccdf0cd6abf78e8a5118fd441534
Bence Ludmány <bence.ludmany@singula.se>
2023-12-27 12:38:34 +0100

    Add lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,9 +2,30 @@ return {
     {
         "folke/noice.nvim",
         event = "VeryLazy",
+        opts = {},
         dependencies = {
             "MunifTanjim/nui.nvim",
             "rcarriga/nvim-notify",
         }
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = 'nightfox',
+                component_separators = '|',
+                section_separators = '',
+            },
+            sections = {
+                lualine_c = {
+                    {
+                        'filename',
+                        file_status = true,
+                        path = 1
+                    }
+                }
+            }
+        },
     }
 }
