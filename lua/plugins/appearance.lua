766380cbcb34c3d922c4ec94cfc85f443627a8c3
Bence Ludmány <bence.ludmany@singula.se>
2023-12-28 18:02:52 +0100

    Make every buffer transparent, add everforest as new theme

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -3,7 +3,43 @@ return {
         "EdenEast/nightfox.nvim",
         priority = 1000,
         config = function()
-            vim.cmd("colorscheme nightfox")
+            -- vim.cmd("colorscheme nightfox")
+        end
+    },
+    {
+        "sainnhe/everforest",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme everforest")
+        end
+    },
+    {
+        "xiyaowong/transparent.nvim",
+        priority = 1000,
+        config = function()
+            local transparent = require('transparent')
+            transparent.clear_prefix('lualine')
+            transparent.clear_prefix('BufferLine')
+            transparent.clear_prefix('NvimTree')
+            transparent.clear_prefix('Notify')
+            transparent.setup({ -- Optional, you don't have to run setup.
+                groups = {      -- table: default groups
+                    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+                    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+                    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+                    'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+                    'EndOfBuffer'
+                },
+                extra_groups = {
+                    -- 'NvimTreeNormal',
+                    -- 'NvimTreeNormalNC',
+                    -- 'NvimTreeRepeat',
+                    -- 'NvimTreeNonText',
+                    -- 'NvimTreeEndOfBuffer',
+                    "NormalFloat"
+                },                   -- table: additional groups that should be cleared
+                exclude_groups = {}, -- table: groups you don't want to clear
+            })
         end
     }
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,15 @@
 return {
     {
+
+        "rcarriga/nvim-notify",
+        config = function()
+            require('notify').setup({
+                -- other stuff
+                background_colour = "#000000"
+            })
+        end
+    },
+    {
         "folke/noice.nvim",
         event = "VeryLazy",
         opts = {},
