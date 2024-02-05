b6d0044c22aeb15b168ebd7d32ab4b0ac01cc82d
Bence Ludmány <bence.ludmany@singula.se>
2024-02-05 12:45:12 +0100

    Remove double space remap

--- lua/bence/remaps.lua
+++ lua/bence/remaps.lua
@@ -32,6 +32,6 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end);
+-- vim.keymap.set("n", "<leader><leader>", function()
+--     vim.cmd("so")
+-- end);
