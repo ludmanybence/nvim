14dbac1e1f748907cda03512ad5598a7e27fe5d7
Bence Ludmány <bence.ludmany@singula.se>
2024-02-05 12:41:33 +0100

    Add flash, no longer hide gitignored items from NvimTree

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -34,7 +34,7 @@ return {
                 },
                 git = {
                     enable = true,
-                    ignore = true,
+                    ignore = false,
                     timeout = 500,
                 },
                 update_focused_file = {
@@ -132,4 +132,18 @@ return {
             },
         },
     },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        vscode = true,
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            -- { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+    }
 }
